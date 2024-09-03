27a11aff50424c60aada76ad6201b04e5c5694c3
Vijay krishna <vijaykrishna.ramagiri@outlook.in>
2024-09-03 14:34:07 +0000

    change

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     restartOnFileChange: true
   });
 };
