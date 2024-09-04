ae9f5f652ca160c7a79e8e81a51676bae11e4c80
vijaykrishna2520 <93523194+vijaykrishna2520@users.noreply.github.com>
2024-09-04 07:51:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sample-o ok';
+  title = 'Hello, okok-app';
 
   public sample():void{
     console.log("sample");
